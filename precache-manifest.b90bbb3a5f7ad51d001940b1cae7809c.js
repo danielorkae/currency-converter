15054a9ff8ec5256da01580e3cfed787b0bc82fd
Daniel Lopes <danielorkae@outlook.com>
2019-03-17 23:46:40 -0300

    efc9c78

---
+++ precache-manifest.b90bbb3a5f7ad51d001940b1cae7809c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ceefeffdd415629c155",
+    "url": "/currency-converter/js/about.39706a00.js"
+  },
+  {
+    "revision": "2e40cba6ac76e3949497",
+    "url": "/currency-converter/js/app.334e8a2e.js"
+  },
+  {
+    "revision": "5ae2a8aa6b25ce41ab26",
+    "url": "/currency-converter/css/chunk-vendors.5137c1e5.css"
+  },
+  {
+    "revision": "5ae2a8aa6b25ce41ab26",
+    "url": "/currency-converter/js/chunk-vendors.35d9d01d.js"
+  },
+  {
+    "revision": "946a72f777c42c1d6fd332f6bc584849",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/currency-converter/robots.txt"
+  }
+];
